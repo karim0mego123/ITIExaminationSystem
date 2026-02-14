56c298f42269619aa82bab66b44194bb890b2c8e
Karim Ayman <karim.ayman.kemo@gmail.com>
2026-02-14 13:53:41 +0200

    Production Version

--- ITIExaminationSystem/Models/ModelView/ChoiceVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ITIExaminationSystem.Models.ModelView
-{
-    public class ChoiceVM
-    {
-        public int ChoiceId { get; set; }
-        public string ChoiceText { get; set; }
-    }
-}
